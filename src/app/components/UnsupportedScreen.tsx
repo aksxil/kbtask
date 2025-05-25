0894af85b91c0734842669239a50dfeef4ac7da3
Aakash Nishad <aksx3u@gmail.com>
2025-05-25 13:09:18 +0530

    fix

--- src/app/components/UnsupportedScreen.tsx
+++ src/app/components/UnsupportedScreen.tsx
@@ -2,7 +2,7 @@ const UnsupportedScreen = () => {
   return (
     <div className="w-full h-screen bg-slate-800 text-white flex flex-col items-center justify-center text-center p-4">
       <h2 className="text-2xl pb-6">🖥️ Not Supported on Mobile</h2>
-        <p>Please use a desktop or larger screen to access the Email Builder.</p>
+        <p>Please use a desktop or larger screen to access this</p>
     </div>
   );
 };
